c28e1c68e173937255fba77c2f7f1acbc1e49c3e
Alex Z <hedgar2017@gmail.com>
2022-06-09 13:20:37 +0200

    Fix the event converter (#3)
    Co-authored-by: Alex Z <a.zarudnyy@matterlabs.dev>

--- src/runners/events.rs
+++ src/runners/events.rs
@@ -102,11 +102,11 @@ pub fn merge_events(events: Vec<EventMessage>) -> Vec<SolidityLikeEvent> {
             let mut buffer = [0u8; 32];
             value.to_big_endian(&mut buffer);
 
-            let topics = if num_topics == 0 {
-                vec![]
+            let (topics, data) = if num_topics == 0 {
+                (vec![], buffer.to_vec())
             } else {
                 num_topics -= 1;
-                vec![buffer]
+                (vec![buffer], vec![])
             };
 
             let new_event = SolidityLikeEvent {
@@ -114,7 +114,7 @@ pub fn merge_events(events: Vec<EventMessage>) -> Vec<SolidityLikeEvent> {
                 tx_number_in_block,
                 address,
                 topics,
-                data: vec![],
+                data,
             };
 
             current = Some((data_length, num_topics, new_event))
